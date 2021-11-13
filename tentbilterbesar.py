95f83cc8ac38350eb87b9a36953ef50198450cee
Hp <nurpratama001@gmail.com>
2021-11-13 09:51:09 -0800

    tentbilterbesar.py telah ditambahkan

---
+++ tentbilterbesar.py
@@ -0,0 +1,11 @@
+#Program menggunakan statement If
+
+kelereng_udin = input("Udin memiliki kelereng sebanyak : ")
+kelereng_budi = input("Budi memiliki kelereng sebanyak : ")
+
+if kelereng_udin == kelereng_udin:
+    if kelereng_udin > kelereng_budi:
+        print("Benar, kelereng Udin yang terbanyak")
+    else :
+        print("Salah, kelereng Budi yang terbanyak")
+
