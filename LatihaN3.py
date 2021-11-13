2db3c681e5d8703f0d12466e0f7f3f3a6709b03b
Hp <nurpratama001@gmail.com>
2021-11-13 09:45:53 -0800

    LatihaN3.py telah ditambahkan

---
+++ LatihaN3.py
@@ -0,0 +1,10 @@
+#Penggunaan kondisi "or"
+
+a = input("Masukan Bilangan A : ")
+b = input("Masukan Bilangan B : ")
+c = input("Masukan Bilangan C : ")
+
+if a+b == c or b+c == a or c+a == b :
+    print("Benar") 
+else :
+    print("Salah")
